73f6cbe25db93a477bf445375aa7c9c7b024e8e2
Shakhemir <shakhemir@gmail.com>
2022-06-20 00:19:48 +0300

    Добавил возможность использования скобок любой сложности

--- task_01_arifmetic.py
+++ task_01_arifmetic.py
@@ -9,7 +9,10 @@ def arith_expression(exp):
         facts = exp.split('*')
         result = 1
         for i in facts:
-            result *= int(i)
+            if '.' in i:
+                result *= float(i)
+            else:
+                result *= int(i)
         return result
 
     def mult_or_div(exp):
@@ -19,6 +22,24 @@ def arith_expression(exp):
             result /= mult(i)
         return result
 
+    "Проверка на скобки. Используем рекурсию для решения"
+    while '(' in exp:
+        "Ищем начало и конец выражения в скобках"
+        index1 = exp.index('(')
+        index2 = index1 + 1
+        count = 1
+        while count > 0:
+            if exp[index2] == '(':
+                count += 1
+            if exp[index2] == ')':
+                count -= 1
+            index2 += 1
+        "Выписываем полученное выражение в отдельную переменную"
+        sub_exp = exp[index1 + 1:index2 - 1]
+        in_brackets = arith_expression(sub_exp)
+        exp = exp[:index1] + str(in_brackets) + exp[index2:]
+
+    "Для удобства приводим все в виде слагаемых. Там где стоит - добавляем спереди +"
     exp = exp.replace('-', '+-').replace(' ', '')
     terms = exp.split('+')
     summ = 0
@@ -28,7 +49,9 @@ def arith_expression(exp):
 
 
 # s = input('Введите арифметическое выражение: ')
-s = '2+3-5*2 - 10/2/5'
+# s = '2 + 3*(1/2 + 4/(5-3)) + 2*((1+2)*3 + 1)'
+s = '2+(1+2)*3 + 3*(1/2 + 4/(5-3))'
+# print(eval(s))
 solution = arith_expression(s)
 print(solution)
-print(solution == eval(s))
+# print(solution == eval(s))
